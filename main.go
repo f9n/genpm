89117a13887d55fecc999d5e5f0eb3197b6a36de
pleycpl <h4rvey@protonmail.com>
2017-06-18 11:51:51 +0000

    added tools commands list

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ type Tool struct {
   installCommand string
   removeCommand  string
   searchCommand  string
+  upgradeCommand string
 }
 
 // run script in bash
@@ -49,16 +50,37 @@ func (this *Tool) search(searchstring string) {
   this.runScript(script)
 }
 
+// https://stackoverflow.com/questions/35343707/linux-apt-get-command-not-found-how-to-install-a-package-in-arch-linux , Thnaks for https://stackoverflow.com/users/523100/czechnology
 var tools = map[string]Tool{
-  "pacman": {
-    "sudo pacman -S",        // install
-    "sudo pacman -R",        // remove
-    "sudo pacman -Ss",       // search
+  "pacman": {               // Arch
+    "sudo pacman -S",         // install
+    "sudo pacman -Rs",        // remove
+    "sudo pacman -Ss",        // search
+    "sudo pacman -Syu",      // upgrade
   },
-  "apt-get": {
-    "sudo apt-get install",  // install
-    "sudo apt-get remove",   // remove
-    "sudo apt-cache search", // search
+  "apt": {                  // Debian/Ubuntu
+    "sudo apt install",
+    "sudo apt remove",
+    "sudo apt search",
+    "sudo apt update; sudo apt upgrade",
+  },
+  "dnf": {                  //  RedHat/Fedora
+    "sudo dnf install",
+    "sudo dnf remove",
+    "sudo dnf search",
+    "sudo dnf upgrade",
+  },
+  "zypper": {               // SLES/openSUSE
+    "zypper install",
+    "zypper remove",
+    "zypper search",
+    "zypper update",
+  },
+  "emerge": {               // Gentoo
+    "emerge [-a]",
+    "emerge -C",
+    "emerge -S",
+    "emerge -u world",
   },
 }
 
@@ -75,24 +97,23 @@ func checkTool(tool string) bool {
 }
 
 func main()  {
-  fmt.Println(tools)
   fmt.Println(tools["pacman"])
   fmt.Println(tools["pacman"].installCommand)
+  fmt.Println(tools["pacman"].removeCommand)
+  fmt.Println(tools["pacman"].searchCommand)
+  fmt.Println(tools["pacman"].upgradeCommand)
   args := os.Args[1:]
 
   switch args[0] {
   case "install":
     tool := tools["pacman"]
     tool.install(args[1])
-    // install(args[1])
   case "remove":
     tool := tools["pacman"]
     tool.remove(args[1])
-    // remove(args[1])
   case "search":
     tool := tools["pacman"]
     tool.search(args[1])
-    //search(args[1])
   default:
     fmt.Println("Not founded!")
   }
