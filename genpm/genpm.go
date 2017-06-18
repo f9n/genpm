3c66aae9f33976fbcb90379ea8cd15dc0c78ca4a
pleycpl <h4rvey@protonmail.com>
2017-06-18 15:03:10 +0000

    created genpm package and move functions

---
+++ genpm/genpm.go
@@ -0,0 +1,28 @@
+package genpm
+
+import (
+  "fmt"
+  "os"
+)
+
+func checkAvailableTools() []string {
+  var availableTools []string
+  for _, tool := range toolsList {
+    if checkTool(tool) {
+      availableTools = append(availableTools, tool)
+    }
+  }
+  return availableTools
+}
+
+// Checking tool with which command
+func checkTool(tool string) bool {
+  cmdOut, err := exec.Command("which", tool).Output()
+	if err != nil {
+		// fmt.Fprintln(os.Stderr, err)
+		return false
+	} else {
+		fmt.Println(string(cmdOut))
+		return true
+	}
+}
