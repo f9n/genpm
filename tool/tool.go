6ae071b947df836a214fcb6f0d371943c568a1ab
pleycpl <h4rvey@protonmail.com>
2017-06-18 16:05:44 +0000

    updated method comment

--- tool/tool.go
+++ tool/tool.go
@@ -16,7 +16,7 @@ type Tool struct {
 
 // run script in bash
 func (this *Tool) runScript(script string) {
-  fmt.Println("[+] Runned Tool runScript method")
+  fmt.Println("[+] Runned runScript method in Tool Struct")
   cmd := exec.Command("/bin/bash", "-c", script)
   cmd.Stdout = os.Stdout
   cmd.Stdin = os.Stdin
@@ -26,28 +26,22 @@ func (this *Tool) runScript(script string) {
 
 // install package with Tool
 func (this *Tool) Install(packagename string) {
-  fmt.Println("[+] Runned Tool install method")
-  fmt.Println(packagename)
+  fmt.Println("[+] Runned Install method in Tool Struct")
   script := this.InstallCommand + " " + packagename
-  fmt.Println(script)
   this.runScript(script)
 }
 
 // remove package with Tool
 func (this *Tool) Remove(packagename string) {
-  fmt.Println("[+] Runned Tool remove method")
-  fmt.Println(packagename)
+  fmt.Println("[+] Runned Remove method in Tool Struct")
   script := this.RemoveCommand + " " + packagename
-  fmt.Println(script)
   this.runScript(script)
 }
 
 // search package with Tool
 func (this *Tool) Search(searchstring string) {
-  fmt.Println("[+] Runned Tool search method")
-  fmt.Println(searchstring)
+  fmt.Println("[+] Runned Search method in Tool Struct")
   script := this.SearchCommand + " " + searchstring
-  fmt.Println(script)
   this.runScript(script)
 }
 
